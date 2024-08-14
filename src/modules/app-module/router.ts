f563761a74fde140eb8179de3a6f4735486f37ae
Mark <mwoyshvillo@gmail.com>
2024-08-14 17:46:30 +0300

    chore(structure): adjust folders structure

--- src/modules/app-module/router.ts
+++ src/modules/app-module/router.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 
-import { AppController } from '@/controllers'
+import * as AppController from './controller'
 
 const appRouter = Router()
 
--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 
-import { appRouter } from '@/routes'
+/* IMPORT ALL YOUR ROUTERS */
+import { appRouter } from '@/modules'
 
 const router = Router()
 
+/* ASSIGN EACH ROUTER TO DEDICATED SUBROUTE */
 router.use('/', appRouter)
 
 export { router }
